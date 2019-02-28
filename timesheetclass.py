ff56fd8071bc654f9cba3c510a41ce4ccac56b24
apartidaSH <47014854+apartidaSH@users.noreply.github.com>
2019-02-28 16:19:40 -0500

    Add files via upload

---
+++ timesheetclass.py
@@ -0,0 +1,12 @@
+import time
+class Timesheet:
+    def __init__(self):
+        self.timetrack = [] #will be the list for the time
+    def start_time(self):
+        self.timetrack.append(self.time.time) #appends start time to the list
+    def end_time(self):
+        self.timetrack.append(self.time.time) #appends end time to the list
+#how do I import employee profile to this?
+
+
+
