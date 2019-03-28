6ae569ef6ba3d5377fcae7d64a3a1fdfd328201f
jambob-shu <46801084+jambob-shu@users.noreply.github.com>
2019-03-28 19:06:09 -0400

    pay and timesheet
    pay and timesheet
    - Alex & Jambo

---
+++ paytime.py
@@ -0,0 +1,23 @@
+    def __init__(self):
+        self.pay = 0
+        self.rate = 0
+        self.hours = 0
+        print("Hello, please enter your rate and hours.")
+    def rate_1(self, hourly_rate):
+        # hourly_rate = float(input("What is your rate: "))
+        self.rate = hourly_rate
+    def hours_1(self):
+        timesheet_hours = float(input("What are your hours for the week: "))
+        self.hours = timesheet_hours
+    def show(self):
+        total = self.rate * self.hours
+        self.pay = total
+# object class
+b = Pay()
+hourly_rate = float(input("What is your rate: "))
+
+# Need to call those functions!
+b.rate_1(hourly_rate)
+b.hours_1()
+b.show()
+print("Your pay is", b.pay)
